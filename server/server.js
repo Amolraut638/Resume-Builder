48b095be915a8ba7dd56a1a52f0e0d0473b9669e
Amol Raut <amolraut9991@gmail.com>
2025-10-22 13:30:10 +0530

    backend server and database configs done

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+
+const app = express();  //created express app 
+const PORT = process.env.PORT || 3000;  // set the PORT number
+
+await connectDB()
+
+//middleware 
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => res.send('Server is live...')) //when we hit the home route ('/') then this funtion will executed
+
+//start the backend server
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
